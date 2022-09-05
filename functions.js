00e7f394f93cacef2a19aec09cd400c5bfc0b467
Winston Calhoun <superdevant@gmail.com>
2022-09-05 10:26:28 -0500

    Add substance

--- functions.js
+++ functions.js
@@ -1,13 +1,42 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
-
+function printGreeting() {
+  console.log("Hello there")
+}
+printGreeting();
+printGreeting();
+printGreeting();
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
+function nameDogs(name){
+  console.log(`My dogs name is ${name}`)
+}
+nameDogs("Pepper");
+nameDogs("Mila");
 
-
-// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company,
+// and the numbers will represent the minimum and maximum of a pay range for a posted job.
+//  The function should print out a sentence that includes the name of the company and the range itself
+// (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+function getSalaryRange(name, min, max){
+  var range = (max - min);
+  console.log(`The range for ${name} is ${range}`)
+}
+getSalaryRange("youtube", 100, 200);
 
 
 // 4: Write a function that satifies the following interaction pattern:
+
+function checkStock(stock, item) {
+  if (stock === 0) {
+    console.log(`${item} is out of stock`);
+  }  else if (stock <= 3) {
+      console.log(`${item} is running low`);
+  }  else if (stock => 4) {
+      console.log(`${item} is stocked`);
+  }
+}
+
+checkStock(4, "Coffee");
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
--- selfEval.md
+++ selfEval.md
@@ -1,6 +1,10 @@
 # Self Evaluation
 
 - Reflect on your time management and awareness of where you spend time. Is this a strength or area you are working on? What support do you need in this area? And/or, what tips do you have to share with your classmates in this area?
+// My time management needs some work.  I either do to much all at once and dont take a break, making me burnt out faster or i dont do enough.
 - How would you explain to a 5 year old what a function is?
+// a piece of Instructions.
 - Share your understanding of arguments and parameters.
+// Aurguments is the entire line.  Parameters inside parentheses.
 - What questions/topics (related to Functions) do you want to being to your Small Group?
+// Questions have already been asked and already been answered.
